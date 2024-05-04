be5843aa8e4b1bf8d1599b2bb5c4e5f2fa30994a
lavanya100890 <yaram.lavanya91@gmail.com>
2024-05-04 19:33:09 +0100

    chnaged App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
       Namaste everyone, let's build Netflix GPT
-      <p>Changes</p>
+      
     </div>
 
-
+    
   );
 }
 
